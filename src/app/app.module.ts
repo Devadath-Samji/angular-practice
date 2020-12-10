7f8a7a94e553623b0e75babe7ba80676617bf122
Devadath-Samji <devadathsamji@gmail.com>
2020-12-10 10:05:31 +0530

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,7 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
 import { ProductListComponent } from './product-list/product-list.component';
-import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
+//import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
 import { CourseComponent } from './course/course.component';
 
 @NgModule({
@@ -21,7 +21,7 @@ import { CourseComponent } from './course/course.component';
     AppComponent,
     TopBarComponent,
     ProductListComponent,
-    ShoppingCartComponent,
+    //ShoppingCartComponent,
     CourseComponent
   ],
   bootstrap: [ AppComponent ]
