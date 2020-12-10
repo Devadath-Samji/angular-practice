4056b5a7599928d4eafea92bf82fdd2f66144ad8
Devadath-Samji <devadathsamji@gmail.com>
2020-12-10 13:47:10 +0530

    4 files modified

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,16 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import {productsArray} from '../productsFile';
 @Component({
   selector: 'app-product-details',
   templateUrl: './product-details.component.html',
   styleUrls: ['./product-details.component.css']
 })
-export class ProductDetailsComponent implements OnInit {
+export class ProductDetailsComponent implements OnInit  {
   //variable to store product which was selected
   product;
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private route:ActivatedRoute) { 
+    console.log("ProductDetailsComponent");
   }
+  //void ngOnInit(){}
+  ngOnInit(): void{
+    this.route.paramMap.subscribe(function(params){
+      this.product = productsArray[+params.get('productId')];
+    });
+   /* Alternate short way is shown below
+   
+    this.route.paramMap.subscribe(params => {
+      productsArray[+params.get('ProductId')]
+    })
+     */
+    console.log("ProductDetailsComponent post construtction ngOnInit");
+  }
+
+
 
 }
